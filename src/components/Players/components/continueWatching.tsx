e18233cc1e4ac04d5359230baea15ee92aff3811
rosch-arthur <rosch.arthur@gmail.com>
2024-10-14 20:57:46 -0300

    fix

--- src/components/Players/components/continueWatching.tsx
+++ src/components/Players/components/continueWatching.tsx
@@ -9,21 +9,23 @@ export const ContinueWatching = ({
 }: ContinueWatchingProps) => {
   return (
     <>
+      {/* Background overlay cobrindo toda a tela */}
       <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
 
+      {/* Modal centralizado e cobrindo toda a tela */}
       <div className="absolute inset-0 flex items-center justify-center z-20">
         <div className="bg-black bg-opacity-75 text-white p-6 rounded-md text-center">
           <p>Você pausou o vídeo</p>
-          <div className="mt-4">
+          <div className="mt-4 flex flex-col sm:flex-row sm:space-x-4">
             <button
               onClick={handleResume}
-              className="mr-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md my-4 sm:my-0"
             >
               Continuar assistindo
             </button>
             <button
               onClick={handleRestart}
-              className="px-4 py-2 bg-gray-500 text-white rounded-md"
+              className="px-4 py-2 bg-gray-500 text-white rounded-md my-4 sm:my-0"
             >
               Assistir do início
             </button>
