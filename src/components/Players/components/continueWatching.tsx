ac39cc4a9eaed540752d8ab0646370a66717c5e6
rosch-arthur <rosch.arthur@gmail.com>
2024-10-14 20:35:47 -0300

    fox

--- src/components/Players/components/continueWatching.tsx
+++ src/components/Players/components/continueWatching.tsx
@@ -9,9 +9,11 @@ export const ContinueWatching = ({
 }: ContinueWatchingProps) => {
   return (
     <>
-      <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+      {/* Background overlay cobrindo toda a tela */}
+      <div className="fixed inset-0 bg-black opacity-50 z-10"></div>
 
-      <div className="absolute inset-0 flex items-center justify-center z-20">
+      {/* Modal centralizado e cobrindo toda a tela */}
+      <div className="fixed inset-0 flex items-center justify-center z-20">
         <div className="bg-black bg-opacity-75 text-white p-6 rounded-md text-center">
           <p>Você pausou o vídeo</p>
           <div className="mt-4">
