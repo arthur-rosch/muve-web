ee704d91d090591578c9a93123fa8bc735104873
rosch-arthur <rosch.arthur@gmail.com>
2024-12-05 00:23:53 -0300

    Fix PlayerWrapper impot

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,45 +1,42 @@
-export * from './layout/sideBar'
-export * from './layout/header'
-export * from './layout/headerFolder'
-export * from './CustomModal'
+export * from "./layout/sideBar";
+export * from "./layout/header";
+export * from "./layout/headerFolder";
+export * from "./CustomModal";
 
-export * from './players/player'
-export * from './players/previews/vsl-player'
-export * from './players/previews/curse-player'
+export * from "./players/player";
+export * from "./players/previews/vsl-player";
+export * from "./players/previews/curse-player";
 
-export * from './ui/card'
-export * from './ui/input'
-export * from './ui/toast'
-export * from './ui/modal'
-export * from './ui/button'
-export * from './ui/checkbox'
-export * from './ui/nav-menu'
-export * from './ui/separator'
-export * from './ui/input-select'
-export * from './ui/scroll-area'
-export * from './ui/tooltip'
-export * from './ui/select'
-export * from './ui/collapsible'
+export * from "./ui/card";
+export * from "./ui/input";
+export * from "./ui/toast";
+export * from "./ui/modal";
+export * from "./ui/button";
+export * from "./ui/checkbox";
+export * from "./ui/nav-menu";
+export * from "./ui/separator";
+export * from "./ui/input-select";
+export * from "./ui/scroll-area";
+export * from "./ui/tooltip";
+export * from "./ui/select";
+export * from "./ui/collapsible";
 
+export * from "./providers/modal-provider";
+export * from "./modals/upgrade-plan/upgrade-modal";
 
+export * from "./testimonials/testimonial-carousel";
 
-export * from './providers/modal-provider'
-export * from './modals/upgrade-plan/upgrade-modal'
+export * from "./video/cardVideo";
 
-export * from './testimonials/testimonial-carousel'
+export * from "./modals/video/CreateVideoModal";
+export * from "./modals/video/CardCreateVideo";
 
-export * from './video/cardVideo'
+export * from "./modals/folder/CreateFolderModal";
+export * from "./modals/folder/CardCreateFolder";
 
-export * from './modals/video/CreateVideoModal'
-export * from './modals/video/CardCreateVideo'
+export * from "./modals/select-video/selectVideoModal";
 
-export * from './modals/folder/CreateFolderModal'
-export * from './modals/folder/CardCreateFolder'
+export * from "./modals/move-video/moveVideoModal";
+export * from "./modals/preview-player/previewPlayerModal";
 
-export * from './modals/select-video/selectVideoModal'
-
-export * from './modals/move-video/moveVideoModal'
-export * from './modals/preview-player/previewPlayerModal'
-
-
-export * from './folder/listFolders'
+export * from "./folder/listFolders";
